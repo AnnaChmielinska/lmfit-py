bbba180a71e7ff5e249c81159bca6839455c9ccc
ACh <anna.chmielinska@uj.edu.pl>
2018-01-03 19:30:24 +0100

    On branch statistic Changes to be committed: 	new file:   lmfit/statistic.py
    Added module with statistic object.

---
+++ lmfit/statistic.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""Provide the Statistic object.
+
+Statistic provides definition of objective function and optimization method for fitting process.
+
+"""
+
+from abc import ABCMeta, abstractmethod
+import warnings
+import numpy as np
+import scipy
+import scipy.linalg
+
+class Statistic():
+    """Abstract class for the Statistic object.
+
+    Parameters
+    ----------
+    name : str
+    optimization_method : str
+        One of: 'nelder', 'powell', 'leastsq'.
+
+    Methods
+    -------
+    objective_func : ndarray
+
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, name, optimization_method):
+        """Initialize a Statistic object.
+
+        Parameters
+        ----------
+        name : str
+        optimization_method : str
+            One of: 'nelder', 'powell', 'leastsq'.
+
+        """
+        self.name = name
+        self.optimization_method = optimization_method
+
+    @abstractmethod
+    def objective_func(self, *args, **kwargs):
+        """Objective function for optimization."""
+        raise NotImplementedError("`Statistic.objective_func` not implemented")
